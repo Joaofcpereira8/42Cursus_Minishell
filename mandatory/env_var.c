551bacfbd7075577ec45c77f227dc126a5f16a54
jofilipe <jofilipe@student.42porto.com>
2023-12-19 12:13:20 +0000

    env.h and env_var.c added

---
+++ mandatory/env_var.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_var.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofilipe <jofilipe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 12:23:33 by jofilipe          #+#    #+#             */
+/*   Updated: 2023/12/06 12:24:58 by jofilipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/env.h"
+
+t_env	*env_list(char *type, char * info)
+{
+	t_env *env;
+
+	env = ft_calloc(1, sizeof(t_env));
+	if (!env)
+		return (NULL);
+	env->type = type;
+	env->info = info;
+}
+
---
+++ mandatory/includes/env.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofilipe <jofilipe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 12:22:52 by jofilipe          #+#    #+#             */
+/*   Updated: 2023/12/06 12:22:52 by jofilipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENV_H
+# define ENV_H
+
+# include "minishell.h"
+
+/**
+ * @brief Struct where will keep env variables
+ *
+ * @param type Name of the env.
+ * @param info Info in each env variable.
+ */
+typedef struct s_env
+{
+	char	*type;
+	char	*info;
+}				t_env;
+
+/**
+ * @brief Creates a new node
+ *
+ * @param type Name of the env.
+ * @param info Info in each env variable.
+ */
+t_env	*env_list(char *type, char * info);
+
+t_env	*add_env();
+#endif
