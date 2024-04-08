29c9d26858c572ef217ce278b131feebafd1f2cc
berestv <bbento-e@student.42porto.com>
2024-04-08 18:23:52 +0100

    fixed echo crashing with double space

--- mandatory/builtins/echo.c
+++ mandatory/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: bbento-e <bbento-e@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:11:51 by bbento-e          #+#    #+#             */
-/*   Updated: 2024/03/21 12:16:36 by bbento-e         ###   ########.fr       */
+/*   Updated: 2024/04/08 18:18:11 by bbento-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int miniecho(char **args)
 	int	i;
 	int nl;
 
-	if (!mini_shell()->input[5] && !mini_shell()->input[6])
+	if ((!mini_shell()->input[5] && !mini_shell()->input[6]) || mini_shell()->input[5] == ' ')
 	{
 		printf("\n");
 		return (-1);
 	}
-
-	//args = ft_split(args[0], ' ');
 	nl = 1;
 	i = 1;
 	if(args[1][0] && args[1][0] == '-' && args[1][1] == 'n')
