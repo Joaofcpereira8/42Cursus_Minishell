a54370ec4adc1f75d87970b6f8bf31d785f1d9b3
berestv <bbento-e@student.42porto.com>
2024-04-18 19:11:19 +0100

    fixed bug: export wasn't printing quotes

--- mandatory/builtins/env.c
+++ mandatory/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: jofilipe <jofilipe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:11:57 by bbento-e          #+#    #+#             */
-/*   Updated: 2024/04/18 12:24:16 by bbento-e         ###   ########.fr       */
+/*   Updated: 2024/04/18 18:31:56 by bbento-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ char	**env_to_mat(t_list *env_list)
 	while (env_list)
 	{
 		env = (t_env *)env_list->content;
-		if (env->info[0] && (env->info[0] != '"' && env->info[1] != '"'))
+		if (env->info || (env->info[0] != '"' && env->info[1] != '"'))
 		{
 			tmp1 = ft_strjoin(env->type, "=");
 			tmp2 = ft_strjoin(tmp1, env->info);
--- mandatory/utils3.c
+++ mandatory/utils3.c
@@ -6,7 +6,7 @@
 /*   By: jofilipe <jofilipe@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 14:57:23 by jofilipe          #+#    #+#             */
-/*   Updated: 2024/04/18 11:33:05 by bbento-e         ###   ########.fr       */
+/*   Updated: 2024/04/18 19:09:57 by bbento-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,45 @@ void	env_join(char **arr1, char *str)
 	// mini_shell()->tenv = envlist();
 }
 
+int	flag_setter(int	*flag)
+{
+	*flag = 1;
+	return (1);
+}
+
+/*char	**lst_to_mat(t_list *list)
+{
+	t_env	*env;
+	char	*tmp1;
+	char	*tmp2;
+	char	**matrix;
+	int 	flag;
+
+	matrix = ft_calloc(1, sizeof(char *));
+	if (!matrix)
+		return (NULL);
+	while (list)
+	{
+		env = (t_env *)list->content;
+		if (env->info[0] == '"')
+		{
+			if (env->info[0] == '"')
+				tmp1 = ft_strjoin(env->type, "=");
+			else if (flag_setter(&flag))
+				tmp1 = ft_strjoin(env->type, "=\"");
+			if (flag == 1)
+				tmp2 = ft_strjoin(tmp1, ft_strjoin(env->info, "\""));
+			else
+				tmp2 = ft_strjoin(tmp1, env->info);
+			matrix = add_to_mat(matrix, ft_strdup(tmp2));
+			free(tmp1);
+			free(tmp2);
+		}
+		list = list->next;
+	}
+	return (matrix);
+}*/
+
 char	**lst_to_mat(t_list *list)
 {
 	t_env	*env;
@@ -76,11 +115,24 @@ char	**lst_to_mat(t_list *list)
 	while (list)
 	{
 		env = (t_env *)list->content;
-		if (env->info[0] == '"')
-			tmp1 = ft_strjoin(env->type, "=");
+		if (env->info[0]) //== '"')
+		{
+			if (env->info[0] != '"')
+			{
+				tmp1 = ft_strjoin(env->type, "=\"");
+				tmp2 = ft_strjoin(tmp1, ft_strjoin(env->info, "\""));
+			}
+			else
+			{
+				tmp1 = ft_strjoin(env->type, "=");
+				tmp2 = ft_strjoin(tmp1, env->info);
+			}
+		}
 		else
+		{
 			tmp1 = ft_strdup(env->type);
-		tmp2 = ft_strjoin(tmp1, env->info);
+			tmp2 = ft_strjoin(tmp1, env->info);
+		}
 		matrix = add_to_mat(matrix, ft_strdup(tmp2));
 		free(tmp1);
 		free(tmp2);
@@ -88,4 +140,3 @@ char	**lst_to_mat(t_list *list)
 	}
 	return (matrix);
 }
-
